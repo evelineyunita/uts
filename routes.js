792ec7d37d48f5c457d452e71fa6e32a5b167db3
Eveline Yunita <evelineyunita@outlook.com>
2020-04-26 19:48:30 +0700

    berhasil mengubah data level

--- routes.js
+++ routes.js
@@ -42,4 +42,7 @@ module.exports = function(app) {
             app.route('/ubahuser')
             .put(jsonku.ubahuser);
 
+            app.route('/ubahlevel')
+            .put(jsonku.ubahlevel);
+
 } 
